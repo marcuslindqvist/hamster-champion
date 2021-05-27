a37e15f9c34fcd65f8111cbb4b00cfa8a5cb6645
marcuslindqvist <marcus.lindqvist@iths.se>
2021-05-27 16:58:10 +0200

    add hamster form closeable

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,11 +1,13 @@
 import GalleryItem from "./GalleryItem"
 import InputModal from "./InputModal"
-
+import {useState} from 'react'
 const Gallery = ({ hamsterList }) => {
+const [modalStatus, setModalStatus] = useState(false)
 
     return (
         <div className="gallery">
-            <InputModal />
+            {modalStatus ? <InputModal closeModal={setModalStatus} /> : <button onClick={() => setModalStatus(true)}>Lägg till hamster</button>}
+            
             <div className="gallery-grid">
                 {hamsterList.map((hamster) =>
                     <GalleryItem key={hamster.id} hamster={hamster} />
--- src/components/Gallery/InputModal.jsx
+++ src/components/Gallery/InputModal.jsx
@@ -1,35 +1,57 @@
 import { useState } from "react"
 import CallToAction from "../Buttons/CallToAction"
 
-const InputModal = () => {
+const InputModal = ({ closeModal }) => {
     const [hamsterName, setHamstername] = useState("hamster")
     const [imgUrl, setImgUrl] = useState("http://localhost:2010/img/flagster.png")
+
     async function addHamster() {
 
     }
     function showName(evt) {
         if (evt.target.value) {
             setHamstername(evt.target.value)
-        } else(setHamstername("hamster"))
+        } else (setHamstername("hamster"))
     }
     return (
         <div className="input-modal">
             <div className="row">
                 <div className="input-fields">
-                    <label for="name">Namn:</label>
-                    <input type="text" name="name" id="" onBlur={(evt) => showName(evt)} />
+                    <label htmlFor="name">Namn:</label>
+                    <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        onBlur={(evt) => showName(evt)}
+                        required />
 
-                    <label for="age">Ålder:</label>
-                    <input type="number" name="age" id="" />
+                    <label htmlFor="age">Ålder:</label>
+                    <input
+                        type="number"
+                        name="age"
+                        id="age"
+                        required />
 
-                    <label for="loves">Älskar att:</label>
-                    <input type="text" name="loves" id="" />
+                    <label htmlFor="loves">Älskar att:</label>
+                    <input
+                        type="text"
+                        name="loves"
+                        id="loves"
+                        required />
 
-                    <label for="favFood">Favoritmat:</label>
-                    <input type="text" name="favFood" id="" />
+                    <label htmlFor="favFood">Favoritmat:</label>
+                    <input
+                        type="text"
+                        name="favFood"
+                        id="favFood"
+                        required />
 
-                    <label for="image-link">Bild-url:</label>
-                    <input type="url" name="image-link" onBlur={(e) => setImgUrl(e.target.value)} />
+                    <label htmlFor="image-link">Bild-url:</label>
+                    <input
+                        type="url"
+                        name="image-link"
+                        id="image-link"
+                        onBlur={(e) => setImgUrl(e.target.value)} required />
                 </div>
                 <div className="input-fields">
 
@@ -42,6 +64,7 @@ const InputModal = () => {
             <CallToAction
                 buttonText={`Lägg till ${hamsterName}`}
                 onClick={addHamster} />
+            <button onClick={() => closeModal(false)}>Stäng</button>
         </div>
     )
 }
